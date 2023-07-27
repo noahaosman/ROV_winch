481d33363b61f37c60205954fd8844f789653429
noahaosman <noahaosman@gmail.com>
2023-07-26 19:19:50 -0700

    debug LWA

--- Actuator.py
+++ Actuator.py
@@ -68,13 +68,13 @@ class Actuator:
         while True:
             time.sleep(0.001)
             current_feedback_value = self.feedback.value
-            if current_feedback_value == 1 and prior_feedback_val == 0:
+            if current_feedback_value == True and prior_feedback_val == False:
                 current_pulse_time = time.time()
-                if (current_pulse_time - self.last_pulse_time) > false_pulse_delay_actuator:  # debouncing
-                    self.position = self.position + 1
-                    self.last_pulse_time = current_pulse_time
-                    print(self.position)
-                    prior_feedback_val = current_feedback_value
+                #if (current_pulse_time - self.last_pulse_time) > false_pulse_delay_actuator:  # debouncing
+                self.position = self.position + 1
+                self.last_pulse_time = current_pulse_time
+                print(self.position)
+                prior_feedback_val = current_feedback_value
             print(current_feedback_value)
 
     # write speed to actuator. 0<=value<=100
