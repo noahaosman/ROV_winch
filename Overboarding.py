8db851773ff303537586564cbf9f81e922af9dc7
noahaosman <noahaosman@gmail.com>
2023-07-28 17:36:51 -0700

    save prior speed & reset after overboarding

--- Overboarding.py
+++ Overboarding.py
@@ -32,6 +32,7 @@ class Switch:
     #   2) retracted LOW , deployed HIGH ::  ROV is in water  --> Motor speed unrestricted
     #   3) retracted LOW , deployed LOW  ::  arm is moving into retracted position --> restrict motor to low speed
     def read_state(self, winch):
+        deployment_speed = 18
         last_state = 0
         while True:
             if self.retracted.value == 1 and self.deployed.value == 0 and last_state != 1:
@@ -42,12 +43,17 @@ class Switch:
 
             elif self.retracted.value == 0 and self.deployed.value == 1 and last_state != 2:
                 print('Arm overboard, no speed restrictions.')
+                if last_state == 3:
+                    winch.move_servo(prior_speed)
                 last_state = 2
 
             elif self.retracted.value == 0 and self.deployed.value == 0:
-                if winch.ON.value == 1 and winch.servo.angle > 18 * 270/100:
+                if winch.ON.value == 1 and winch.servo.angle > deployment_speed * 270/100:
                     print('Arm retracting, slowing motor down.')
-                    winch.move_servo(18)
+                    prior_speed = winch.servo.angle * 100/270
+                    winch.move_servo(deployment_speed)
+                else:
+                    prior_speed = deployment_speed
                 last_state = 3
 
             elif self.retracted.value == 1 and self.deployed.value == 1: 
